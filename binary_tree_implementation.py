486d86b78a70084bab93af33923857377561d61a
sagnikrana <sagnik.rana1994@gmail.com>
2019-09-07 08:12:17 -0400

    binary tree traversal

---
+++ binary_tree_implementation.py
@@ -0,0 +1,46 @@
+# Binary tree implementation
+
+class Node(object):
+	def __init__(self, value):
+		self.value = value
+		self.left = None
+		self.right = None
+
+class BinaryTree(object):
+	def __init__(self,root):
+		self.root = Node(root)
+
+	def print_tree(self, traversal_type):
+		if traversal_type == 'preorder':
+			return self.preorder_print(tree.root,"")
+
+		else:
+			print("Traversal type "+str(traversal_type)+" is not supported")
+			return False
+
+	def preorder_print(self, start, traversal):
+		'''
+		Root -> left sub tree -> check if null -> if has left then
+		print, otherwise right 
+		'''
+
+		if start:
+			traversal += (str(start.value) + '-')
+			traversal = self.preorder_print(start.left, traversal)
+			traversal = self.preorder_print(start.right, traversal)
+		return traversal
+# 		    1
+# 		  /   \
+# 		 2     3
+# 		/ \   / \
+ #	   4   5 6   7
+
+tree = BinaryTree(1)
+tree.root.left = Node(2)
+tree.root.right = Node(3)
+tree.root.left.left = Node(4)
+tree.root.left.right = Node(5)
+tree.root.right.left = Node(6)
+tree.root.right.right = Node(7)
+
+tree.print_tree('preorder')
---
+++ remove_element.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+
+	Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+	Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+	The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+	Example 1:
+
+	Given nums = [3,2,2,3], val = 3,
+
+	Your function should return length = 2, with the first two elements of nums being 2.
+
+	It doesn't matter what you leave beyond the returned length.
+        """
+        counter = 0
+        for i in range(len(nums)):
+            i = i - counter
+            if nums[i] == val:
+                counter = counter + 1
+                del nums[i]
+        return len(nums)
+        
+		
+		
+if __name__ == '__main__':
+	result = Solution.twoSum(Solution, [-3,4,3,90], 2)
+	print(result3
