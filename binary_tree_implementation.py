8d6b6642de1dff36536e88b7cfd2912f96c5c0da
sagnikrana <sagnik.rana1994@gmail.com>
2019-09-07 08:18:50 -0400

    binary tree traversals

--- binary_tree_implementation.py
+++ binary_tree_implementation.py
@@ -1,34 +1,51 @@
 # Binary tree implementation
 
 class Node(object):
-	def __init__(self, value):
-		self.value = value
-		self.left = None
-		self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
 
 class BinaryTree(object):
-	def __init__(self,root):
-		self.root = Node(root)
+    def __init__(self, root):
+        self.root = Node(root)
 
-	def print_tree(self, traversal_type):
-		if traversal_type == 'preorder':
-			return self.preorder_print(tree.root,"")
+    def print_tree(self, traversal_type):
+        if traversal_type == "preorder":
+            return self.preorder_print(tree.root, "")
+        elif traversal_type == "inorder":
+            return self.inorder_print(tree.root, "")
+        elif traversal_type == "postorder":
+            return self.postorder_print(tree.root, "")
 
-		else:
-			print("Traversal type "+str(traversal_type)+" is not supported")
-			return False
+        else:
+            print("Traversal type " + str(traversal_type) + " is not supported.")
+            return False
 
-	def preorder_print(self, start, traversal):
-		'''
-		Root -> left sub tree -> check if null -> if has left then
-		print, otherwise right 
-		'''
+    def preorder_print(self, start, traversal):
+        """Root->Left->Right"""
+        if start:
+            traversal += (str(start.value) + "-")
+            traversal = self.preorder_print(start.left, traversal)
+            traversal = self.preorder_print(start.right, traversal)
+        return traversal
 
-		if start:
-			traversal += (str(start.value) + '-')
-			traversal = self.preorder_print(start.left, traversal)
-			traversal = self.preorder_print(start.right, traversal)
-		return traversal
+    def inorder_print(self, start, traversal):
+        """Left->Root->Right"""
+        if start:
+            traversal = self.inorder_print(start.left, traversal)
+            traversal += (str(start.value) + "-")
+            traversal = self.inorder_print(start.right, traversal)
+        return traversal
+
+    def postorder_print(self, start, traversal):
+        """Left->Right->Root"""
+        if start:
+            traversal = self.postorder_print(start.left, traversal)
+            traversal = self.postorder_print(start.right, traversal)
+            traversal += (str(start.value) + "-")
+        return traversal
 # 		    1
 # 		  /   \
 # 		 2     3
@@ -43,4 +60,6 @@ tree.root.left.right = Node(5)
 tree.root.right.left = Node(6)
 tree.root.right.right = Node(7)
 
-tree.print_tree('preorder')
+print("Preorder traversal is : ",tree.print_tree("preorder"))
+print("Inorder traversal is : ",tree.print_tree("inorder"))
+print("Postorder traversal is : ",tree.print_tree("postorder"))
