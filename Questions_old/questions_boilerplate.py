61577b76f12a38a4043c3e96eaaf48cad5800f54
sagnikrana <sagnik.rana1994@gmail.com>
2021-10-11 20:44:28 -0400

    Updating files

--- Questions_old/questions_boilerplate.py
+++ Questions_old/questions_boilerplate.py
@@ -2,58 +2,21 @@ import collections
 
 
 class Solution:
-
-    def __init__(self):
-        self.counter = 0
-
-    def canPlaceFlowers(self, flowerbed, n):
-        if len(flowerbed) == 1:
-            if 0 in flowerbed and n <= 1:
-                return True
-            else:
-                return False
-
-        if len(flowerbed) == 2:
-            if 1 in flowerbed or n > 1:
-                return False
-            else:
-                return True
-
-        if len(flowerbed) >= 3:
-            if 0 not in flowerbed:
-                return False
-            first_available = flowerbed.index(0)
-            if first_available == 0:
-                if flowerbed[first_available + 1] in [0,2]:
-                    flowerbed[first_available] = 1
-                    flowerbed[first_available + 1] = 2
-                    self.counter += 1
-                    if self.counter == n:
-                        return True
-                    else:
-                        self.canPlaceFlowers(flowerbed, n)
-
-            # print(first_available)
-            elif flowerbed[first_available - 1] in [0, 2] and flowerbed[first_available + 1] in [0, 2] and first_available > 0:
-                flowerbed[first_available - 1] = 2
-                flowerbed[first_available + 1] = 2
-                flowerbed[first_available] = 1
-                print(flowerbed)
-                self.counter += 1
-                if self.counter == n:
-                    return True
-                else:
-                    self.canPlaceFlowers(flowerbed, n)
-            elif flowerbed[first_available - 1] == 1 or flowerbed[first_available + 1] == 1:
-                flowerbed[first_available] = 2
-                self.canPlaceFlowers(flowerbed, n)
-
-        if self.counter == n:
+    def canAttendMeetings(self, intervals):
+        if len(intervals) <= 1:
             return True
-        else:
-            return False
+
+        for index in range(len(intervals)):
+            interval = intervals[index]
+            intervals.remove(interval)
+            intervals.insert(0, interval)
+            if index != len(intervals):
+                for rest_other_interval in intervals[1:]:
+                    if interval[0] < rest_other_interval[0] < interval[1] or interval[0] < rest_other_interval[1] < interval[1] or interval[0]==rest_other_interval[0] or rest_other_interval[1]==interval[1]:
+                        return False
+        return True
 
 
 if __name__ == '__main__':
     sol = Solution()
-    print(sol.canPlaceFlowers(flowerbed=[1,0,0,0,1,0,0], n=2))
+    print(sol.canAttendMeetings([[9, 10], [4, 9], [4, 17]]))
