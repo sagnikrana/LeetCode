9a32a3755e2332ea9976bf76b2c534d143c82688
Sagnik Rana <sagnik.rana1994@gmail.com>
2021-12-29 10:48:38 -0500

    Time: 34 ms (7.27%), Space: 13.4 MB (66.17%) - LeetHub

---
+++ combination-sum-iii/combination-sum-iii.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def combinationSum3(self, k,n):
+        results = []
+        def backtrack(remain, comb, next_start):
+            if remain == 0 and len(comb) == k:
+                # make a copy of current combination
+                # Otherwise the combination would be reverted in other branch of backtracking.
+                results.append(list(comb))
+                return
+            elif remain < 0 or len(comb) == k:
+                # exceed the scope, no need to explore further.
+                return
+
+            # Iterate through the reduced list of candidates.
+            for i in range(next_start, 9):
+                comb.append(i+1)
+                backtrack(remain-i-1, comb, i+1)
+                # backtrack the current choice
+                comb.pop()
+
+        backtrack(n, [], 0)
+
+        return results
