4bfb3d9eb5f30085f48da8830da9810505722c71
Sagnik Rana <sagnik.rana1994@gmail.com>
2021-12-30 17:02:58 -0500

    Time: 744 ms (20.83%), Space: 0B (100.00%) - LeetHub

---
+++ department-highest-salary/department-highest-salary.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+with stat as (
+select name, departmentId, salary,
+rank() over (partition by departmentId order by salary desc) as salary_rank
+from Employee)
+
+select d.name as Department,
+stat.name as Employee,
+stat.salary as Salary from Department d left join stat
+on d.id = stat.departmentId where salary_rank = 1;
