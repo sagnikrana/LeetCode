f5e3ff8f2c63bf549d1dbea0b8159dd09cd04505
Sagnik Rana <sagnik.rana1994@gmail.com>
2022-01-08 17:29:12 -0500

    Time: 276 ms (66.06%), Space: 0B (100.00%) - LeetHub

--- 570-managers-with-at-least-5-direct-reports/570-managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/570-managers-with-at-least-5-direct-reports.sql
@@ -1,14 +1,14 @@
 # Write your MySQL query statement below
 
-# with stat as (select managerId
-# from Employee
-# group by managerID
-# having count(managerId) > 4)
-
-# select name from Employee where id in (select distinct managerId as managerId from stat);
-
-
-select name from Employee emp inner join (select managerId
+with stat as (select managerId
 from Employee
 group by managerID
-having count(managerId) > 4) res on emp.id = res.managerId;
+having count(managerId) > 4)
+
+select name from Employee where id in (select distinct managerId as managerId from stat);
+
+
+# select name from Employee emp inner join (select managerId
+# from Employee
+# group by managerID
+# having count(managerId) > 4) res on emp.id = res.managerId;
