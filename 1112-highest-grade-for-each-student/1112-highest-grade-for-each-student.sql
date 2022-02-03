5435d61dd50a85a23111635727251ebdd0a66733
Sagnik Rana <sagnik.rana1994@gmail.com>
2022-02-03 13:01:13 -0500

    Time: 453 ms (89.14%), Space: 0B (100.00%) - LeetHub

--- 1112-highest-grade-for-each-student/1112-highest-grade-for-each-student.sql
+++ 1112-highest-grade-for-each-student/1112-highest-grade-for-each-student.sql
@@ -1,11 +1,22 @@
 # Write your MySQL query statement below
 
-with stat as (select student_id, course_id, grade,
+# with stat as (select student_id, course_id, grade,
+# row_number() over (partition by student_id order by grade desc, course_id) as grade_rank
+# from Enrollments)
+
+# select student_id, course_id, grade
+# from stat
+# where grade_rank = 1
+# order by student_id
+# ;
+
+
+
+with res as (select student_id, course_id, grade,
 row_number() over (partition by student_id order by grade desc, course_id) as grade_rank
 from Enrollments)
 
-select student_id, course_id, grade
-from stat
+select student_id, course_id, grade from res
 where grade_rank = 1
 order by student_id
 ;
